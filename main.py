6c394a91709a5ae69a514b9d5e1dc368fe65539e
Rodrigo Stange Tessinari <qa18147@bristol.ac.uk>
2019-03-11 21:06:35 +0000

    First working version

--- main.py
+++ main.py
@@ -6,59 +6,57 @@
 """
 
 import sys
-from os import walk
 from os import path
 import subprocess
 
-def run(root_folder):
-    num_dir = 0
-    num_files = 0
-    fail_flag = False
-
-    try:
-        open('missing_files.txt','w')
-    except:
-        print('Could not open file... this is odd.')
-
-    with open('result.txt', 'w+') as resultFile:    
-        for (dirpath, dirnames, filenames) in walk(root_folder):
-            num_dir = num_dir + len(dirnames)
-            num_files = num_files + len(filenames)
-
-            resultFile.write('Pasta ' + dirpath + '\n\n')
-            for file in filenames:
-                try:
-                    with open(dirpath+'\\'+file) as data:
-                        resultFile.write("Arquivo \'%s\':\n%s\n" % (file,data.read()))
-                except:
-                    with open('missing_files.txt','a') as missingFiles:
-                        missingFiles.write(dirpath+'\\'+file)
-                        fail_flag = True
-
-            resultFile.write('\n')
-            resultFile.write('\n')
-    if fail_flag:
-        print('Failed to open the following files:')
-        try:
-            with open('missing_files.txt','r') as missingFiles:
-                print(missingFiles.read())
-        except:
-            print('Could not open \'missing_files.txt\'... this is odd.')
-    else:
-        print('All folders read =)')
-
 if __name__ == "__main__":
     root_folder = ''
-    if (len(sys.argv) != 2):
-        root_folder = path.join('home','yang','models','allModels')
-        print(root_folder)
-        print("You did not choose a folder.")
-        print(subprocess.check_output("ls %s".format(root_folder)))
+    file_lst = []
+    explored_lst = []
+    result_dict = {}
+    unexplored_lst = []
+
+    if (len(sys.argv) < 3):
+        # root_folder = path.join('/','home','sdn','yang','models','allModels')
+        print("You are not doing this right. argv[1] == path to model, argv[2:] .yang list.")
         sys.exit()
     else:
         root_folder = sys.argv[1]
-    print("Using \"%s\" as root folder." % (root_folder))
-    
-    run(root_folder)
-    
-    print('Job done.')
+        file_lst = sys.argv[2:]
+
+    unexplored_lst = file_lst.copy()
+
+    print('input file list:',file_lst)
+    while len(unexplored_lst) != 0:
+        print('explored files:',explored_lst)
+        print('dependency list:',unexplored_lst)
+        
+        command_lst = unexplored_lst.copy()
+        command = 'pyang -f depend {} | cut -d : -f 2'.format(str.join(' ',command_lst))
+        # print(command)
+        print()
+
+        result = str(subprocess.check_output(command, cwd=root_folder, shell=True),'utf-8')
+        result_lst = result.split('\n')
+        explored_lst = explored_lst + unexplored_lst[:]
+        unexplored_lst.clear()
+        
+        for i in range(0,len(command_lst)):
+            aux = result_lst[i].split(' ')
+            for j in aux:
+                if j != '' and j != ' ':
+                    yang_file = j+".yang"
+                    if yang_file not in explored_lst and yang_file not in unexplored_lst:
+                        unexplored_lst.append(yang_file)
+                    if command_lst[i] not in result_dict:
+                        result_dict[command_lst[i]] = []
+                    result_dict[command_lst[i]].append(yang_file)
+        # sys.exit()
+
+    print('Results:')
+    print('input file list:',file_lst)
+
+    print('Dependency list:')
+    for key in result_dict.keys():
+        print('  {}: {}'.format(key,result_dict[key]))
+    print('Files needed:',explored_lst)
